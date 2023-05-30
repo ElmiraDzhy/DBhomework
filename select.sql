d8b7a7367061722ef5fdcdc6fe1107a3d1d219a6
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-30 12:33:43 +0300

    Select workers

---
+++ select.sql
@@ -0,0 +1,31 @@
+SELECT *
+FROM workers
+WHERE id = 3;
+
+SELECT *
+FROM workers
+WHERE salary > 400;
+
+SELECT salary, extract(years from age(birthday))
+FROM workers
+WHERE first_name = 'Jenya';
+
+SELECT *
+FROM workers
+WHERE first_name = 'Petya';
+
+SELECT *
+FROM workers
+WHERE extract(years from age(birthday)) >= 27
+  AND salary < 1000;
+
+SELECT *
+FROM workers
+WHERE extract(years from age(birthday)) BETWEEN 25 AND 30;
+
+
+SELECT *
+FROM workers
+WHERE salary > 300
+  AND length(first_name) < 6;
+
