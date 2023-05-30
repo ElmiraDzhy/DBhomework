24d313e3dbc6d1d7515c7fe7fd96e97d55c2af16
ElyaDzhy <elmira.freshcode@gmail.com>
2023-05-30 12:19:35 +0300

    Add four workers

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO workers (first_name, last_name, salary, birthday)
+VALUES ('Oleg', 'Sirko', 300, '1990-05-06');
+
+INSERT INTO workers (first_name, last_name, salary, birthday)
+VALUES ('Yaroslava', 'Tokar', 1200, '1979-06-06');
+
+INSERT INTO workers (first_name, last_name, salary, birthday)
+VALUES ('Sasha', 'Serduk', 1000, '1985-12-03'),
+       ('Masha', 'Serduk', 900, '1995-07-24');
